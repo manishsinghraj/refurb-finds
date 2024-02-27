45ccd187d5eb23887e2b7a2b8087990c66cc3599
Manish Singh <manishpurohit97@gmail.com>
2024-02-27 12:20:24 +0530

    header & footer

--- client/README.md
+++ client/README.md
@@ -1,5 +1,52 @@
 # Client
+<hr>
 
 # Package installation
 
-`npm i axios redux react-redux redux-thunk react-router-dom @redux-devtools/extension react-helmet`
+`npm i axios redux react-redux redux-thunk react-router-dom @redux-devtools/extension react-helmet react-icons`
+
+<hr>
+
+# scss setup
+
+https://github.com/manishsinghraj/learn-sass
+
+- create folder `assets` in src
+- `assets` > `scss` > `global Folder` - `_index.scss`, `_boilerplate.scss`
+- `assets` > `scss` > `utils Folder` - `_index.scss`, `_font.scss`
+- `assets` > `scss` > `style.scss`
+
+watch scss and build it
+
+`import './build/style.css'` in App.js
+
+<hr>
+
+# pages
+
+- create Home.js
+- create Shop.js
+
+
+# components
+
+- create components folder
+- create header folder - Header.js
+- create footer folder - Footer.js
+- create layout folder - Layout.js
+
+# router
+
+- create router folder
+- create Route.js - link all pages and create path.
+
+
+# connect
+
+- App.js add Layout.js component 
+<br>
+
+ 
+![alt text](image.png)
+
+<hr>
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { IoSearchSharp } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { LuShoppingCart } from "react-icons/lu";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { NavLink } from "react-router-dom"
+
+
+const navLinks = [
+  {
+    path: "home",
+    name: "Home"
+  },
+  {
+    path: "shop",
+    name: "Shop"
+  },
+  {
+    path: "cart",
+    name: "Cart"
+  }
+]
+
+const Header = () => {
+  return (
+    <>
+      <div className='container'>
+
+        <header className='header'>
+
+          <div className='header__logo'>
+            <img src="refurb-logo.png" alt='refurb-logo'></img>
+            <h1 className='header__logo-text'>Refurb<span className='header__logo-text-span'>Finds</span></h1>
+          </div>
+
+          <div className='header__search'>
+            <div className='header__search__box'>
+              <IoSearchSharp className='header__search__box-icon' />
+              <input type='text' placeholder='Search for anything' className='header__search__box-input'></input>
+            </div>
+          </div>
+
+        </header>
+
+        <nav className='header__navigation'>
+          <ul className='header__navigation__menu'>
+            {
+              navLinks.map((item, index) =>
+              (<li key={index} >
+                <NavLink to={item.path} className={(navClass) => navClass.isActive ? "header__navigation__active" : ""}>{item.name}</NavLink>
+              </li>
+              ))
+            }
+          </ul>
+
+          <div className='header__navigation__icons'>
+            <span className='header__navigation__icons__cart'><LuShoppingCart />
+              <span className='header__navigation__icons__cart__badge'>20</span>
+            </span>
+            <span className='header__navigation__icons__like'><FaRegHeart />
+              <span className='header__navigation__icons__like__badge'>20</span>
+            </span>
+            <span className='header__navigation__icons__userlogo'><FaRegCircleUser /></span>
+          </div>
+        </nav>
+
+      </div>
+    </>
+  )
+}
+
+export default Header
